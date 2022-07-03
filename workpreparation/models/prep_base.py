7795b477c9cc73476d6f27974f98a3bc20adaa5f
Florian Dubois <fds@mbpro.local>
2022-07-03 11:39:08 +0200

    added drawing field

--- workpreparation/models/prep_base.py
+++ workpreparation/models/prep_base.py
@@ -1,5 +1,6 @@
 from importlib.metadata import files
 from itertools import product
+import string
 from odoo import fields, models
 
 class PrepModel(models.Model):
@@ -9,3 +10,5 @@ class PrepModel(models.Model):
     name = fields.Char()
     description = fields.Char()
     product_id = fields.Many2one("product.template", string="Product")
+    bom_id = fields.Many2one("product.bom", string="BOM")
+    drawing_pdf = fields.Many('ir.attachment', string="Drawing")
